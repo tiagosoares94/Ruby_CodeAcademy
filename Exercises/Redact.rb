18f429fcc6fea725b12c56b16c1f4b8fdb38830f
Tiago Soares <tsoares.sjc@gmail.com>
2018-04-09 16:59:35 -0300

    new exercise added

---
+++ Exercises/Redact.rb
@@ -0,0 +1,18 @@
+=begin
+ Lesson 2: Exercise to learn to hide some user information about users inputs    
+=end
+
+puts "Text to search through: "
+text = gets.chomp
+puts "Word to redact: "
+redact = gets.chomp
+
+words = text.split(" ")
+
+words.each do |word|
+  if word != redact
+    print word + " "
+  else
+    print "REDACTED "
+  end
+end
